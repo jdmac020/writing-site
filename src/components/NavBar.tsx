4923339f0378810a2b5a2a260108f9803c8c3879
John MacGregor <john.macgregor@gmail.com>
2024-09-01 19:27:33 -0400

    Add Home link to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,7 +12,8 @@ export const NavBar = () => {
             <Box component={'img'} src={logo} sx={{ maxWidth: { xs: '30%', lg: '9%' }, marginRight: { md: '2rem', lg: '6rem' } }}/>
             <Typography variant={'h4'} sx={{ fontSize: { md: '3rem', lg: '2rem' }}}>John's Writing</Typography>
           </Grid>
-          <Grid container flex={'flex'} direction={'row'} justifyContent={'space-around'} alignContent={'center'} width={{ xs: '60%'}}>
+          <Grid container flex={'flex'} direction={'row'} justifyContent={'space-around'} alignContent={'center'} width={{ xs: '80%'}}>
+            <ResponsiveLink target={'/'} text={'Home'} />
             <ResponsiveLink target={'/stories'} text={'Stories'} />
             <ResponsiveLink target={'/about'} text={'About'} />
           </Grid>
