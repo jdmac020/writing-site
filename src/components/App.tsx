0ce3aa81a7832a03345b0ad846c828191291abbf
John MacGregor <john.macgregor@gmail.com>
2024-09-01 19:25:54 -0400

    Add placeholder stories page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './Home';
 import { MainLayout } from './MainLayout';
+import { TableOfContents } from './TableOfContents';
 
 export const App = () => {
   return (
@@ -10,6 +11,7 @@ export const App = () => {
         <Routes>
           <Route path={'/'} element={<MainLayout />}>
             <Route index element={<Home />} />
+            <Route path={'/stories'} element={<TableOfContents />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,8 +6,9 @@ export const Home = () => {
   return (
     <Grid container flex={'flex'} alignContent={'center'} direction={'column'}>
       <H1 text={"John's Story Spot"}/>
+      {/* desktop image sized container */}
       <Grid container flex={'flex'} marginTop={'1rem'}>
-        {/* left page */}
+        {/* left page, a component to extract */}
         <Grid container flex={'flex'} direction={'column'} width={'50%'} paddingLeft={'2.5rem'} paddingRight={'1.5rem'}>
           <Body1 text={"Left page is where, in English, the whole thing gets started. It's wild, it's crazy, it is absolutely a crazy time."} />
           <Body1 text={"People will arrive at the left page and be absolutely sucked into some sort of crazy adventure and never want to leave."} />
@@ -17,7 +18,7 @@ export const Home = () => {
           <Body1 text={"Right page is where the action gets continued. It's where an Act 2 happens, it's where we see the danger, the sadness."} />
           <Body1 text={"Or in our case right now, it's where I'll explain what the hell is going on."} />
         </Grid>
-        {/* right page */}
+        {/* right page, another component to extract */}
         <Grid container flex={'flex'} direction={'column'} width={'50%'} paddingLeft={'1.5rem'} paddingRight={'2.5rem'}>
           <Body1 text={"This is a placeholder for a new website where I'm going to combine the technical skillz and the writing skillz in one place. I'm going to self publish some writing in the most ass way possible!"} />
           <Body1 text={"The home page won't always look like this -- I'm just trying to identify what the actual reading pages should look like. So. Bear with me while I hammer out all the nitty gritty details like how many words can reasonably fit on one of these pages."} />
