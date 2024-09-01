26fed00955f34d2bffaf260035aaf7bde94a3a65
John MacGregor <john.macgregor@gmail.com>
2024-09-01 12:37:20 -0400

    Settle navbar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,29 @@
-import { AppBar, Toolbar, Grid } from '@mui/material';
+import { AppBar, Toolbar, Grid, Typography } from '@mui/material';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import logo from '../assets/logo.jpg';
+import styled from '@emotion/styled';
 
 export const NavBar = () => {
   return (
     <AppBar position={'static'}>
-      <Toolbar sx={{ background: 'white' }}>
-        <Grid container justifyContent={'flex-start'} sx={{ marginTop: '.5rem' }}>
-          <Link to={'/'}>
-            <img src={logo} height={'70'} width={'70'} />
-          </Link>
-        </Grid>
-        <Grid container justifyContent={'space-around'} width={'75%'}>
-          <Link to={'/'}>About Me</Link>
-          <Link to={'/'}>My Links</Link>
-          <Link to={'/'}>Photos</Link>
+      <Toolbar>
+        <Grid container flex={'flex'} direction={'row'} justifyContent={'space-around'} wrap={'nowrap'}>
+          <Typography variant={'h4'} align={'center'}>John's Writing</Typography>
+          <Grid container flex={'flex'} direction={'row'} justifyContent={'space-around'} width={'60%'}>
+            <BolderLink to={"/stories"}>Stories</BolderLink>
+            <BolderLink to={'/about'}>About</BolderLink>
+          </Grid>
         </Grid>
       </Toolbar>
     </AppBar>
   );
 };
+
+const BolderLink = styled(NavLink)`
+  color: white;
+  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+  font-size: 2.125rem;
+  letter-spacing: 0.00735em;
+  line-height: 1.235;
+`
