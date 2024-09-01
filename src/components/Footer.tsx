2214dc7e5aa5536ffa3d04a1059b104f6b0cbf8d
John MacGregor <john.macgregor@gmail.com>
2024-09-01 14:52:19 -0400

    Fix footer to be a footer not a header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,19 @@
 import styled from '@emotion/styled';
-import { AppBar, Grid, Toolbar, Typography } from '@mui/material'
+import { AppBar, BottomNavigation, Grid, Toolbar, Typography } from '@mui/material'
 import React from 'react';
 
 const year = new Date().getFullYear();
 
 export const Footer = () => {
-  return <AppBar position={'static'}>
-    <Toolbar>
-      <Grid container flex={'flex'} direction={'row'} justifyContent={'space-around'}>
-        <Grid container direction={'column'}>
-          <Typography alignSelf={'center'}>&copy; {year} Johnathan MacGregor</Typography>
-          <WhiteLink href={'mailto:john.macgregor@gmail.com'}>Contact</WhiteLink>
-        </Grid>
+  return <BottomNavigation>
+      <Grid container flex={'flex'} width={'35%'} alignItems={'center'} justifyContent={'space-evenly'} color={'white'}>
+        <Typography>&copy; {year} Johnathan MacGregor</Typography>
+        <WhiteLink href={'mailto:john.macgregor@gmail.com'}>Contact</WhiteLink>
       </Grid>
-    </Toolbar>
-  </AppBar>;
+  </BottomNavigation>;
 }
 
 const WhiteLink = styled('a')`
   font-family: "Roboto", "Helvetica", "Arial", sans-serif;
-  text-align: center;
   color: white;
 `;
--- src/style.tsx
+++ src/style.tsx
@@ -3,10 +3,10 @@ import { createTheme } from '@mui/material/styles';
 // this isn't actually doing anything for me -- but I'm being lazy and want the example in code
 export const theme = createTheme({
   components: {
-    MuiToolbar: {
+    MuiBottomNavigation: {
       styleOverrides: {
         root: {
-          justifyContent: 'space-between'
+          backgroundColor: 'rgb(25, 118, 210)'
         }
       }
     }
